22ae56ab885e2b0df21773bcec9692171848c1bf
Deblyn Prado <deblynprado@hotmail.com>
2025-02-01 10:50:51 -0300

    Exercise1

---
+++ conceitos/variaveis.py
@@ -0,0 +1,35 @@
+#**Cadastro de Usuário**
+#Descrição: Crie variáveis para armazenar o nome de um usuário (string), sua idade (int), e se ele está ativo no sistema (bool). 
+#Em seguida, imprima uma mensagem de boas-vindas que inclua o nome do usuário e a confirmação de que ele está ativo.
+#Objetivo: Praticar a criação de variáveis do tipo `string`, `int`, e `bool`.
+
+
+
+#**Registro de Produto**
+#Descrição: Crie variáveis para armazenar o nome de um produto (string), a quantidade em estoque (int), 
+# o preço por unidade (float) e se ele está disponível para venda (bool). Em seguida, imprima uma mensagem com todas essas informações.
+
+
+#**Cadastro de Evento**
+#Descrição: Crie variáveis para armazenar o nome de um evento (string), a data do evento (string), o número de 
+# participantes esperados (int), e se o evento está com inscrições abertas (bool). Imprima uma mensagem com todas essas informações.
+
+
+
+
+#**Perfil de Cliente**
+#Descrição: Crie variáveis para armazenar o nome de um cliente (string), sua idade (int), seu saldo em conta (float), 
+# e se ele tem cartão de crédito ativo (bool). Em seguida, imprima uma mensagem com as informações do cliente.
+
+
+
+
+#**Cadastro de Funcionário**
+#Descrição: Crie variáveis para armazenar o nome de um funcionário (string), seu número de identificação (int), 
+# seu salário (float), e se ele está atualmente empregado (bool). Imprima uma mensagem contendo todas essas informações.
+
+
+
+#**Registro de Veículo**
+#Descrição: Crie variáveis para armazenar a marca de um carro (string), o ano de fabricação (int), 
+# o preço de venda (float), e se ele está disponível no estoque (bool). Em seguida, imprima uma mensagem com os detalhes do veículo.
