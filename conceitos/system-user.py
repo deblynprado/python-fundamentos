09ea45a29e390ca2105ffdaf90a027351942d192
Deblyn Prado <deblynprado@hotmail.com>
2025-02-01 10:43:39 -0300

    System to check if user is active or not

---
+++ conceitos/system-user.py
@@ -0,0 +1,17 @@
+# Crie variáveis para armazenar um nome de usuario, sua idade, se esta ativo no sistema.
+# Em seguida imprima uma mensagem de boas vindas com todos os dados salvos
+
+name = "Deblyn Prado"
+age = 29
+is_active = True
+
+print("\n=====================================")
+print("***** Welcome to the System *****")
+print("Username: ", name)
+print("Age: ", age)
+if is_active:
+    print("This user is active in the system")
+else:
+    print("This user is inactive in the system")
+print("=====================================")
+
