5b9cfcfb55d0ae956e3b4c3b35a91b7591e33c4f
Deblyn Prado <deblynprado@hotmail.com>
2025-02-08 09:41:46 -0300

    Adding for loop

--- conceitos/aula1/exercicios.py
+++ conceitos/aula1/exercicios.py
@@ -4,22 +4,41 @@ lastName = input("Your Last Name: ")
 city = input("Your City: ")
 
 print("What are your 3 favorite hobbies?")
-hobby1 = input("Hobby 1: ")
-hobby2 = input("Hobby 2: ")
-hobby3 = input("Hobby 3: ")
+hobby = []
+hobby.append(input("Hobby 1: "))
+hobby.append(input("Hobby 2: "))
+hobby.append(input("Hobby 3: "))
 
 print("What are the 3 places that you'd like to visit?")
-travel1 = input("Place 1: ")
-travel2 = input("Place 2: ")
-travel3 = input("Place 3: ")
+places = []
+places.append(input("Place 1: "))
+places.append(input("Place 2: "))
+places.append(input("Place 3: "))
 
 print("What are your 3 favorite foods?")
-food1 = input("Food 1: ")
-food2 = input("Food 2: ")
-food3 = input("Food 3: ")
+foods = []
+foods.append(input("Food 1: "))
+foods.append(input("Food 2: "))
+foods.append(input("Food 3: "))
 
 print("Alright", firstName, lastName, "from", city, "!")
-print("It looks like we have a lot of information about you.\n")
-print("Here are your hobbies: \n", hobby1, "\n", hobby2, "\n", hobby3)
-print("Here are the places you'd like to visit: \n", travel1, "\n", travel2, "\n", travel3)
-print("Here are your favorite foods: \n", food1, "\n", food2, "\n", food3)
+print("It looks like we have a lot of information about you.")
+print("Here are your hobbies:")
+count = 0
+for i in hobby:
+  count = count+1
+  print(count, "-", i)
+print()
+
+print("Here are the places you'd like to visit:")
+count = 0
+for i in places:
+  count = count+1
+  print(count, "-", i)
+print()
+
+print("Here are your favorite foods:")
+count = 0
+for i in foods:
+  count = count+1
+  print(count, "-", i)
